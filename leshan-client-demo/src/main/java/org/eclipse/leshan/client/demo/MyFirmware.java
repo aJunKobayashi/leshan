92d66c21a230b8981bc22a3e6a997f4b1715ac37
jun.kobayashi <jun.kobayashi@access-company.com>
2021-04-27 16:40:14 +0900

    refactor.

--- leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/MyFirmware.java
+++ leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/MyFirmware.java
@@ -10,6 +10,7 @@ import org.eclipse.leshan.core.response.WriteResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Resource;
 import java.util.*;
 
 
@@ -29,6 +30,61 @@ public class MyFirmware extends BaseInstanceEnabler {
     private String mPkgName = "leshanPkg";
     private String mPkgVersion = "1.0.9";
 
+    private enum ResourceID {
+        PACKAGE(0),
+        PACKAGE_URI(1),
+        UPDATE(2),
+        STATE(3),
+        UPDATE_RESULT(5),
+        PACKAGE_NAME(6),
+        PACKAGE_VERSION(7),
+        FIRMWARE_UPDATE_DELIVERY_METHOD(9),
+        UNKNOWN(-1);
+
+        private final int val;
+        ResourceID(int i) {
+            val = i;
+        }
+
+        public static ResourceID convertID(int resourceID) {
+            for (ResourceID id: ResourceID.values()) {
+                if (id.val == resourceID) {
+                    return id;
+                }
+            }
+            return UNKNOWN;
+        }
+    }
+
+    private enum StateEnum {
+        IDLE(0),
+        DOWNLOADING(1),
+        DOWNLOADED(2),
+        UPDATING(3);
+
+        public final int val;
+        StateEnum(int i) {
+            val = i;
+        }
+    }
+
+    private enum UpdateResultEnum {
+        Initial(0),
+        FirmwareUpdated(1),
+        NotEnoughMemory(2),
+        ConnectionLost(4),
+        IntegrityCheckError(5),
+        UnsupportedPackageType(6),
+        InvalidUrl(7),
+        FirmwareUpdateFailed(8),
+        UnsupportedProtocol(9);
+
+        public final int val;
+        UpdateResultEnum(int i) {
+            val = i;
+        }
+    }
+
 
 
     public MyFirmware() {
@@ -47,35 +103,28 @@ public class MyFirmware extends BaseInstanceEnabler {
     @Override
     public ReadResponse read(ServerIdentity identity, int resourceid) {
         LOG.info("Read on Firmware resource /{}/{}/{}", getModel().id, getId(), resourceid);
-        switch (resourceid) {
-            case 0:
+        ResourceID id = ResourceID.convertID(resourceid);
+        switch (id) {
+            case PACKAGE:
                 byte[] byteArray = new byte[mPackage.size()];
                 for (int i = 0; i < mPackage.size(); i++) {
                     byteArray[i] = mPackage.get(i);
                 }
                 return ReadResponse.success(resourceid, byteArray);
-            case 1:
+            case PACKAGE_URI:
                 return ReadResponse.success(resourceid, mPackageURI);
-            case 3:
+            case STATE:
                 return ReadResponse.success(resourceid, mState);
-            case 5:
+            case UPDATE_RESULT:
                 return ReadResponse.success(resourceid, mUpdateResult);
-            case 6:
+            case PACKAGE_NAME:
                 return ReadResponse.success(resourceid, mPkgName);
-            case 7:
+            case PACKAGE_VERSION:
                 return ReadResponse.success(resourceid, mPkgVersion);
-            case 9:
+            case FIRMWARE_UPDATE_DELIVERY_METHOD:
                 return ReadResponse.success(resourceid, mFirmwareUpdateDeliveryMethod);
             default:
                 return super.read(identity, resourceid);
-        }
-    }
-
-    class UpdateTask extends TimerTask
-    {
-        public void run()
-        {
-
         }
     }
 
@@ -85,8 +134,8 @@ public class MyFirmware extends BaseInstanceEnabler {
             @Override
             public void run() {
                 LOG.info("Read on Firmware State is changed to Downloading");
-                self.mState = 1;
-                fireResourcesChange(3);
+                self.mState = StateEnum.DOWNLOADING.val;
+                fireResourcesChange(ResourceID.STATE.val);
                 self.updateDownloadedState();
             }
         }, 1_000);
@@ -98,8 +147,8 @@ public class MyFirmware extends BaseInstanceEnabler {
             @Override
             public void run() {
                 LOG.info("Read on Firmware State is changed to Downloaded");
-                self.mState = 2;
-                fireResourcesChange(3);
+                self.mState = StateEnum.DOWNLOADED.val;
+                fireResourcesChange(ResourceID.STATE.val);
             }
         }, 30_000);
     }
@@ -107,8 +156,9 @@ public class MyFirmware extends BaseInstanceEnabler {
     @Override
     public WriteResponse write(ServerIdentity identity, int resourceid, LwM2mResource value) {
         LOG.info("Write on Firmware resource /{}/{}/{}", getModel().id, getId(), resourceid);
-        switch(resourceid) {
-            case 1:
+        ResourceID id = ResourceID.convertID(resourceid);
+        switch(id) {
+            case PACKAGE_URI:
                 mPackageURI = value.getValue().toString();
                 updateDownloadingState();
                 return WriteResponse.success();
@@ -120,15 +170,16 @@ public class MyFirmware extends BaseInstanceEnabler {
     @Override
     public ExecuteResponse execute(ServerIdentity identity, int resourceid, String params)  {
         LOG.info("Execute on Firmware resource /{}/{}/{}", getModel().id, getId(), resourceid);
-        switch (resourceid) {
-            case 2:
+        ResourceID id = ResourceID.convertID(resourceid);
+        switch (id) {
+            case UPDATE:
                 try {
                     Thread.sleep(10_000);
-                    mUpdateResult = 1;
-                    mState = 0;
+                    mUpdateResult = UpdateResultEnum.FirmwareUpdated.val;
+                    mState = StateEnum.IDLE.val;
                     mPkgName = "Updated LeshanPkg";
                     mPkgVersion = "2.0.0";
-                    fireResourcesChange(3, 5, 6, 7);
+                    fireResourcesChange(ResourceID.STATE.val, ResourceID.UPDATE_RESULT.val, ResourceID.PACKAGE_NAME.val, ResourceID.PACKAGE_VERSION.val);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
