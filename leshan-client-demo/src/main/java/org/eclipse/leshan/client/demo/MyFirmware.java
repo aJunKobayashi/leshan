b360fc8d7d449bc8cf1cc7a3e0e3a96838a9a191
jun.kobayashi <jun.kobayashi@access-company.com>
2022-03-29 09:47:11 +0900

    fix build error

--- leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/MyFirmware.java
+++ leshan-client-demo/src/main/java/org/eclipse/leshan/client/demo/MyFirmware.java
@@ -10,7 +10,6 @@ import org.eclipse.leshan.core.response.WriteResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Resource;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
